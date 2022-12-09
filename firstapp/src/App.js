8c2d864319006d12b5e1af41b6178f7207711d2d
HtetMinPaing <hmphtetminpaing@gmail.com>
2022-12-09 12:29:57 +0630

    addign the first react app

---
+++ firstapp/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Header from './Heading';
+
+function App() {
+  return (
+    <div className="App">
+      This is the starting code for "Your first component" ungraded lab
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ firstapp/src/Heading.js
@@ -0,0 +1,5 @@
+function Header(){
+    return <h1>This is an h1 heading</h1> ;
+}
+
+export default Header;
